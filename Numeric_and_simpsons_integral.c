404d253a195368874131799bacda3da467b43f7f
Arda Yıldız <129327270+ardayildiz4@users.noreply.github.com>
2023-03-29 23:11:25 +0300

    Update Numeric_and_simpsons_integral.c

--- Numeric_and_simpsons_integral.c
+++ Numeric_and_simpsons_integral.c
@@ -1,31 +1,27 @@
+//This code consists of 2 functions, simpson and numeric parts.
+//They integrate sin(x) function in the interval (a,b) by using the Simpson's Rule and also numeric method.
+//You enter the boundaries a and b in float type, when the function is called.
+
 #include <stdio.h>
 #include <math.h>
 
 float integrate_simpson(float a,float b);
 float integrate_numeric(float a,float b);
-int main()
+
+float integrate_simpson(float a,float b)//DO NOT CHANGE arguments, name and return type
 {
-    float integrate_simpson(float a,float b)//DO NOT CHANGE arguments, name and return type
+    float integral=(b-a)/6*(sin(a)+4*sin((a+b)/2)+sin(b));
+    return integral;
+}
+float integrate_numeric(float a,float b)//DO NOT CHANGE arguments, name and return type
+{
+    float n=102000000000;
+    float sum=0;
+    float h=fabs(b-a)/n;
+    for(int i=1;i<n;i++)
     {
-        float integral=(b-a)/6*(sin(a)+4*sin((a+b)/2)+sin(b));
-        return integral;
+        float x=a+i*h;
+        sum+=sin(x);
     }
-    float integrate_numeric(float a,float b)//DO NOT CHANGE arguments, name and return type
-    {
-        float n=102000000000;
-        //dy/dx=sinx
-        //dy= sinx*dx
-        //y = integral sinx*dx
-        float sum=0;
-        float h=fabs(b-a)/n;
-        for(int i=1;i<n;i++)
-        {
-            float x=a+i*h;
-            sum+=sin(x);
-        }
-        return sum;
-    }
-  printf("integral: %f",integrate_simpson(0,3.14/2));
-  printf("integral: %f",integrate_numeric(0,3.14/3));
-    return 0;
+    return sum;
 }
